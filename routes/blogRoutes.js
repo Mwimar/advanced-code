ef4ca6ba1ac5c2d29ad78b6a8fa56100a52e720d
Mark Mwima <markmwimar@gmail.com>
2023-11-12 18:14:33 +0300

    signup logic

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -17,6 +17,44 @@ router.get("/signup", function (req, res) {
   res.render("signup");
 });
 
+router.post("/signup", async function (req, res) {
+  const user = req.body;
+  const enteredEmail = user.email;
+  const enteredConfirmEmail = user.confirmemail;
+  const enteredPassword = user.password;
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  if (
+    !enteredEmail ||
+    !enteredConfirmEmail ||
+    email !== enteredConfirmEmail ||
+    !enteredPassword ||
+    enteredPassword.trim() < 6
+  ) {
+    req.session.userData = {
+      hasError: true,
+      message: "Invalid User Data",
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword,
+    };
+
+    req.session.save(function () {
+      res.redirect("/signup");
+    });
+  }
+
+  const existingUser = db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  const userData = {
+    email: email,
+    password: hashedPassword,
+  };
+});
+
 router.get("/login", function (req, res) {
   res.render("login");
 });
