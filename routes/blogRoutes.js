a3b7671153b7b1bd35f4da5bdc3eeff9e54fdac2
Mark Mwima <markmwimar@gmail.com>
2023-11-12 17:41:06 +0300

    styles setup

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -9,4 +9,24 @@ const db = require("../data/database");
 const ObjectId = mongodb.ObjectId;
 const router = express.Router();
 
+router.get("/", function (req, res) {
+  res.redirect("/admin");
+});
+
+router.get("/signup", function (req, res) {
+  res.render("signup");
+});
+
+router.get("/login", function (req, res) {
+  res.render("login");
+});
+
+router.get("/admin", function (req, res) {
+  res.render("admin");
+});
+
+router.get("/logout", function (req, res) {
+  res.redirect("/login");
+});
+
 module.exports = router;
