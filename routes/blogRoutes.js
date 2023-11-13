078517b2526f77066749924d6bdb95095c7ed38b
Mark Mwima <markmwimar@gmail.com>
2023-11-13 15:53:59 +0300

    configusring posts routes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -197,4 +197,67 @@ router.post("/logout", function (req, res) {
   res.redirect("/");
 });
 
+router.get("/posts/:id/edit", async function (req, res) {
+  const postId = new ObjectId(req.params.id);
+  const post = await db.getDb().collection("posts").findOne({ _id: postId });
+
+  if (!post) {
+    return res.render("404");
+  }
+
+  let sessionInputData = req.session.inputData;
+
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      title: post.title,
+      content: post.content,
+    };
+  }
+  req.session.inputData = null;
+
+  res.render("single-post", {
+    post: post,
+    inputData: sessionInputData,
+  });
+});
+
+router.post("/posts/:id/edit", async function (req, res) {
+  const enteredTitle = req.body.title;
+  const enteredContent = req.body.content;
+  const postId = new ObjectId(req.params.id);
+
+  if (
+    !enteredTitle ||
+    !enteredContent ||
+    enteredTitle.trim() === "" ||
+    enteredContent.trim() === ""
+  ) {
+    req.session.inputData = {
+      hasError: true,
+      message: "invalid Input",
+      title: enteredTitle,
+      content: enteredContent,
+    };
+
+    req.redirect(`/posts/${req.params.id}/edit`);
+    return;
+  }
+
+  await db
+    .getDb()
+    .collection("posts")
+    .updateOne(
+      { _id: postId },
+      { $set: { title: enteredTitle, content: enteredContent } }
+    );
+  res.redirect("/admin");
+});
+
+router.post("/posts/:id/delete", async function (req, res) {
+  const postId = new ObjectId(req.params.id);
+  await db.getDb().collection("posts").deleteOne({ _id: postId });
+  res.redirect("/admin");
+});
+
 module.exports = router;
